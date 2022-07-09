1b396ac9cd142a5c7bb3eeb735bdbd29423c29d3
panda1 <blueblue0011@gmail.com>
2022-07-09 15:00:37 -0700

    added while loop that printed user if atleast 18

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 let user_names = [`Maisy`, `Josie`, `Lana`, `Joan`, `Hana`, `Warren`, `Harley`, `Ben`, `Jake`, `Kye`];
 let user_ages = [14, 18, 13, 11, 27, 24, 21, 7, 3, 22];
 
+let counter = 0;
+while (counter < user_ages.length) {
+    if (user_ages[counter] >= 18) {
+        console.log(`${user_names[counter]} you're atleast 18 years old`)
+    }
+    counter++;
+}
